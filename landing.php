924f79c1960556989499a707a7a70c4e52ac1a62
lohanisangya <72278317+lohanisangya@users.noreply.github.com>
2021-11-16 20:05:56 -0500

    Add files via upload

--- landing.php
+++ landing.php
@@ -3,16 +3,14 @@
 <head>
 
 <?php
-session_start();
 include("bootstrap.php");
-include("landing_util.php");
-
+include("proj_util.php");
 include_once("db_connect.php");
 ?>
 
 <!--script src="https://kit.fontawesome.com/2020037322.js" crossorigin="anonymous"></script-->
 
-<link href="landing_style.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">
 
 <title>Lost and Found</title>
 
@@ -40,25 +38,28 @@ include_once("db_connect.php");
         
         <div class="col-md-2 name">
             <a href="#" class="pst-name">John Smith </a> <!-- need to make a php function that gets name from uid-->
-  	</div>
-  	
-  	<div class="col-md-1 settings">
-    	Settings <!--need a logo-->
-  	</div>
+      </div>
+      
+      <div class="col-md-1 settings">
+        Settings <!--need a logo-->
+      </div>
     </div>
-   	 
+       
    <div class="row">
+   <div class="col-md-2 spaceCol">
+   </div>
        <div class="col-md-2 column post_button">
            <a href="./submitFound.php">I Found an Item</a>
        </div>
        
        <div class="col-md-2 column post_button">
-           <a href="./submitLost.php">I lost and item</a>
+           <a href="./submitLost.php">I lost an Item</a>
        </div>
     </div>
     
     <?php showFeedPost($db); ?>
-    
+   
 </div>
 </body>
 </html>
+
--- submitFound.php
+++ submitFound.php
@@ -1,18 +1,18 @@
-<html>
-<head>
+<HTML>
+<HEAD>
 
 <?php
-    include("bootstrap.php");
+	include("bootstrap.php");
 ?>
-<title> Found Form </title>
+<TITLE> Found Form </TITLE>
 
 <style>
-    .space-row{
-   	 height:150px;
-    }
+	.space-row{
+		height:150px;
+	}
 </style>
 
-</head>
+</HEAD>
 <body bgColor="#FFFFFF">
 
 <div class="space-row">
@@ -21,30 +21,27 @@
 
 <h2 align="center">Form for Found Items</h2>
 
-<!-- when submitted, form1.php is executed to
- 	handle the form inputs and return the results to user -->
+<!-- when submitted, form1.php is executed to 
+     handle the form inputs and return the results to user -->
 
-<form name="fmExample" method="POST" action="fmProcess.php">
+<form name="foundFm" method="POST" action="fmProcess.php">
 
-<table align="center" border="1" cellspacing="0" cellpadding="4"
-   	style="border: solid 1px !important;">
+<table align="center" border="1" cellspacing="0" cellpadding="4" 
+       style="border: solid 1px !important;">
 
 
-<!-- row 1: header row -->
-
-<!-- row 2: text field -->
+<!-- row 1: Name of item -->
 <tr>
 <td>Name of the item</td>
-<td><input type="text" name="tf" placeholder="enter text"></td>
+<td><input type="text" name="tfName" placeholder="enter text"></td>
 </tr>
 
-<!-- row 3: description-->
 <tr>
-<td>Description of the item</td>
-<td><textarea name="tarea" value="" rows="5" cols="60"></textarea></td>
+<td>Item Type</td>
+<td><input type="text" name="itemtype" placeholder="enter text"/> <p style="font-size:10; margin-top:5px;"> *Example: Phone, Medicine, Driver License. </p></td>
 </tr>
 
-<!-- row 7: drop down list -->
+<!-- row 2: drop down category -->
 <tr>
 <td>Category</td>
 <td>
@@ -59,18 +56,36 @@
 </td>
 </tr>
 
+<tr>
+<td>Found Location</td>
+<td><input type="text" name="foundLoc" placeholder="Where did you find it?"/></td>
+</tr>
+
+<tr>
+<td>Current Location</td>
+<td><input type="text" name="curLoc" placeholder="Where is it now?"/></td>
+</tr>
+
 <!---------ELECTRONIC SPECIFIC SECTION----------------------->
 <!-- row 3: electronic specific brand-->
 <tr>
 <td>Brand</td>
-<td><input type="text" name="tf" placeholder="enter text"></textarea></td>
+<td><input type="text" name="tfBrand" placeholder="enter text"></td>
 </tr>
 <tr>
 <td>Color</td>
-<td><input type="text" name="tf" placeholder="enter text"></textarea></td>
+<td><input type="text" name="tfColor" placeholder="enter text"></td>
 </tr>
 
 <!--------------------------------------------------->
+
+
+<!-- row 3: description-->
+<tr>
+<td>Description of the item</td>
+<td><textarea name="tArea" value="" rows="5" cols="60"></textarea></td>
+</tr>
+
 
 
 <!-- row 8: drop down list, multiple choice -->
@@ -81,17 +96,18 @@
 </td>
 </tr>
 </table>
-</form>
 
+</br>
 <div class="row">
 <div class="col-md-4"></div>
 <div class="col-md-2">
-<input type="submit" value="Submit Form" />
+<input type="submit" value="Submit Form"/>
 </div>
 <div class="col-md-2">
 <input type="reset" value="Clear Form" />
 </div>
 </div>
+</form>
 
 </body>
-</html>
+</HTML>
