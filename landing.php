7da73a11e476673425af37a17c0f1db923eed950
Tom <cocplayer1alpha@gmail.com>
2021-11-20 15:01:16 -0500

    add feature to a landing page without userID

--- landing.php
+++ landing.php
@@ -3,14 +3,15 @@
 <head>
 
 <?php
+session_start();
 include("bootstrap.php");
-include("proj_util.php");
+include("landing_util.php");
 include_once("db_connect.php");
 ?>
 
 <!--script src="https://kit.fontawesome.com/2020037322.js" crossorigin="anonymous"></script-->
 
-<link href="css/style.css" rel="stylesheet" type="text/css">
+<link href="landing_style.css" rel="stylesheet" type="text/css">
 
 <title>Lost and Found</title>
 
@@ -32,17 +33,22 @@ include_once("db_connect.php");
         <div class="col-md-1 spaceCol">
         </div>
         
-        <div class="col-md-1 profileCol">
-            <img id="profile" src="res/bg.png" alt="Cannot find picture" width="55" height="55">
-        </div>
-        
-        <div class="col-md-2 name">
-            <a href="#" class="pst-name">John Smith </a> <!-- need to make a php function that gets name from uid-->
-      </div>
-      
-      <div class="col-md-1 settings">
-        Settings <!--need a logo-->
-      </div>
+        <?php
+            if($_SESSION['uid'] == null) {
+                print "<div class='col-md-4 name'><a href='login.php'>Log In </a></div>";
+            }
+            else {
+                $name = $_SESSION['uid'];
+                
+                print "<div class='col-md-1 profileCol'>" . 
+                        "<img id='profile' src='res/bg.png' alt='No picture' width='55' height='55'>" . 
+                        "</div>";
+                print "<div class='col-md-2 name'>" . 
+                        "<a href=#>" . getName($db, $name) . "</a>" . //TODO make page user.php?id=$name
+                        "</div>";  
+                print "<div class='col-md-1 settings'> Settings </div>";
+            }
+        ?> 
     </div>
        
    <div class="row">
