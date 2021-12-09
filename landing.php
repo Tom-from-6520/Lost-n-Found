825b6b5044290d398bc4c4b70cb4fc4f57985e15
Tom <cocplayer1alpha@gmail.com>
2021-12-09 09:21:47 -0500

    add search func

--- landing.php
+++ landing.php
@@ -29,21 +29,40 @@ include("handleButtons.php");
 
 <body>
 <div class="container">
+    <form name='searchForm' method='POST' action='landing.php?op=search'> 
     <div class="row header">
         <div class="col-md-1">
             <img src="res/logo.png" alt="Cannot find logo" width="75" height="75">
         </div>
         
-        <div class="col-md-5 search-box">
-            <input id="search-txt" type="text" name="" placeholder="Type to search" />
+        <div class="col-md-1 input-field">
+        <select name="ddlStatus" required>
+        <option value="F">Found Item</option>
+        <option value="L">Lost Item</option>
+        </select>
+        </div>
+        <div class="col-md-1 input-field">
+        <select name="ddlCategory" required>
+        <option value="all">All</option>
+        <option value="electronics">Electronics</option>
+        <option value="clothing">Clothing</option>
+        <option value="school_supp">School Supplies</option>
+        <option value="personal">Personal Items</option>
+        <option value="misc">Miscellaneous</option>
+        </select>
+        </div>
+        <div class="col-md-3 search-box">
+            <input id="search-txt" type='text' name='tfTerm' placeholder="Type to search"/>
+        </div>
+        <div class="col-md-1 input-field">
+        <img src='./res/search-logo.png' width="27" height="27" onClick="submit()">
+        <input type="hidden" name="action" value="Submit Form"/>
         </div>
         
-        <div class="col-md-1 spaceCol">
-        </div>
 
         <?php
             if($_SESSION['uid'] == null) {
-                print "<div class='col-md-4 name'><a href='login.php'>Log In </a></div>";
+                print "<div class='col-md-3 name'><a href='login.php'>Log In </a></div>";
             }
             else {
                 $name = $_SESSION['uid'];
@@ -60,6 +79,7 @@ include("handleButtons.php");
             }
         ?> 
     </div>
+    </form>
        
    <div class="row">
    <div class="col-md-2 spaceCol">
@@ -73,17 +93,22 @@ include("handleButtons.php");
        </div>
     </div>
     
-    <?php showFeedPost($db); ?>
-  
-   
    <?php
-   
    	$op = $_GET['op'];
    	
    	if($op == "delete"){
    		deletePost($db);
+   	} 
+   	if($op == "search"){
+   	    print "<center>SEARCH RESULT</center>";
+            $status = $_POST['ddlStatus'];
+            $terms = $_POST['tfTerm']; 
+            $category = $_POST['ddlCategory'];
+            searchPost($db, $status, $category, $terms);
+   	} 
+   	else { 
+   	    showFeedPost($db);
    	}
-   
    ?>
 </div>
 </body>
--- landing_util.php
+++ landing_util.php
@@ -18,105 +18,108 @@ function showFeedPost($db){
     $res = $db->query($str);
     
     if($res != false && $res->rowCount() > 0){
-    	
-    	$i = 0;
-    	$m = 0;	
+    	$i = 0;	
    	 while($row = $res->fetch()){
    	     $item_id = $row['id'];
    	     $poster_id = $row['poster_id']; 
    	     $category = $row['category'];
    	     $name = getName($db, $poster_id);
-   	     
-   	     //styling for the name and header of each post
-   	     print "<div class='row post'>";
-   	     print "<div class='col-md-.5'><img id= 'profile' src='res/bg.png' alt='Cannot find picture' width='35' height='35'></div>\n";
-   	     print "<div class='col-md-3 post-name'><a href='profile.php'> $name </a></div>\n";
-   	     print "<div class='col-md-5 post'></div>\n";
-		if($poster_id == $_SESSION['uid']) {
-			print "<div class='col-md-1'>";
-			print "<a href=./postEdit.php?id=$item_id>Edit</a>\n";
-			print "</div>";
-			
-			print "<div class='col-md-1'>";
-			print "<div class='col-md-1'><a href='deletePost.php?op=landing&id=$item_id&cat=$category'>Delete</a></div>\n";
-			print "</div>";
-		}
-   	     print "</div>";
-   	     
-   	     print "<div class = 'row post'>";
    	     
    	     $str = "SELECT * FROM (SELECT * FROM items WHERE id=$item_id) AS I JOIN desc_$category ON id=item_id;\n";
    	     $res_row = $db->query($str);
    	     $row_full = $res_row->fetch();
    	     $detail = $row_full['detail'];
-   	     print "&nbsp &nbsp &nbsp &nbsp$detail<br/>\n";
-   	     print "</div>";
-         
-         $uid = $_SESSION['uid'];
-         $btnName = "claimBtn" . $i;
-         $i++;
-         print "<div class = 'row post'><div class='col-md-12'><br/></div></div>";
-         print "<div class ='row post'><div class = 'col-md-1'></div><div class='col-md-10'>------------------------------------------------------------------------</div></div>\n";
-         print  "<div class = 'row post'>";
-         print "<div class = 'col-md-1'></div>";
-          	if($_SESSION['uid'] == null) {
-          		$i = 0;
-               	print "<div class='col-md-2' align ='center'><a href='login.php'>Claim</a></div>";
-         		print "<div class='col-md-1'> | </div>";
-         		print "<div class='col-md-2'><a href='login.php'>Message</a></div>";  
-         		print "</div>"; 
-         		print "<div class ='row post'><div class = 'col-md-1'></div><div class='col-md-10'>------------------------------------------------------------------------</div></div>\n";
-            }
-            
-            	else{
-         		print "<div class='col-md-2' align ='center' id='$btnName'><a href='landing.php?id=$item_id&uid=$uid'>Claim</a></div>\n";
-         		print "<script>document.getElementById('$btnName').addEventListener('click', confirmClaim); </script>\n";
-         		print "<div class='col-md-1'> | </div>";
-         		print "<div class='col-md-2'><a href='handleButtons.php?op=sendmsg&sendid=$poster_id'>Message</a></div>\n";  
-         		print "</div>"; 
-         		print "<div class ='row post'><div class = 'col-md-1'></div><div class='col-md-10'>------------------------------------------------------------------------</div></div>\n";
-   		 }
-   		 print "<div class = 'row'><div class='col-md-12'><br/></div></div>\n";
+   	     
+   	     writePost($name, $poster_id, $item_id, $category, $detail, $i);
   	  }
 	}
 }
 
-function showPersonalPost($db){
-    
-    $id= $_SESSION['uid'];
-    $str = "SELECT id, category, poster_id FROM items WHERE poster_id='$id' ORDER BY org_date DESC;\n";
-    $res = $db->query($str);
-    
-    if($res != false && $res->rowCount() > 0){
-   	 while($row = $res->fetch()){
-   	     $item_id = $row['id'];
-   	     $poster_id = $row['poster_id']; 
-   	     $category = $row['category'];
-   	     $name = getName($db, $poster_id);
-   	     
-   	     //styling for the name and header of each post
-   	     print "<div class='row post'>\n";
-   	     print "<div class='col-md-.5'><img id= 'profile' src='res/bg.png' alt='Cannot find picture' width='35' height='35'></div>\n";
-   	     print "<div class='col-md-3 post-name'><a href='profile.php'> $name </a></div>\n";
-   	     print "<div class='col-md-2 post'></div>\n";
-	     print "<div class='col-md-1'>\n";
-	     print "<a href=./postEdit.php?id=$item_id>Edit</a>\n";
-	     print "</div>";
-			
-	     print "<div class='col-md-1'>\n";
-	     print "<div class='col-md-1'><a href='deletePost.php?op=profile&id=$item_id&cat=$category'>Delete</a></div>\n";
-	     print "</div>\n";
-   	     
-   	     print "<div class = 'row post'>\n";
-   	     
-   	     $str = "SELECT * FROM (SELECT * FROM items WHERE id=$item_id AND poster_id='$id') AS I JOIN desc_$category ON id=item_id;\n";
-   	     $res_row = $db->query($str);
-   	     $row_full = $res_row->fetch();
-   	     $detail = $row_full['detail'];
-   	     print "&nbsp &nbsp &nbsp &nbsp$detail<br/>\n";
-   	     print "</div>\n";
+function searchPost($db, $status, $categories, $terms) {
+    if($categories == "all") {
+        $cats = array("electronics", "clothing", "personal", "school_supp", "misc");
+        for($i = 0; $i < count($cats); $i++) {
+            searchSingleCategory($db, $status, $cats[$i], $terms);
+        }
     }
-  }
+    else {
+        searchSingleCategory($db, $status, $categories, $terms);
+    }
+    
+}
+
+function searchSingleCategory($db, $status, $category, $terms) {
+    $str = "";
+    if($category == "clothing" || $category == "personal") {
+        $str = "SELECT * FROM items JOIN desc_$category ON id=item_id WHERE status='$status' AND (item_type LIKE '%$terms%' OR detail LIKE '%$terms%');";
+    }
+    else {
+        $str = "SELECT * FROM items JOIN desc_$category ON id=item_id WHERE status='$status' AND (name LIKE '%$terms%' OR detail LIKE '%$terms%');";
+    }
+    $res = $db->query($str);
+    if($res != false && $res->rowCount() > 0){
+    	$i = 0;
+        while($row = $res->fetch()){
+            $item_id = $row['id'];
+   	    $poster_id = $row['poster_id']; 
+   	    $category = $row['category'];
+   	    $name = getName($db, $poster_id);
+   	    $detail = $row['detail'];
+   	    
+   	    writePost($name, $poster_id, $item_id, $category, $detail, $i);
+        }
+    }
+}
+
+
+function writePost($name, $poster_id, $item_id, $category, $detail, $i) {
+
+//styling for the name and header of each post
+print "<div class='row post'>";
+print "<div class='col-md-.5'><img id= 'profile' src='res/bg.png' alt='Cannot find picture' width='35' height='35'></div>\n";
+print "<div class='col-md-3 post-name'><a href='profile.php'> $name </a></div>\n";
+print "<div class='col-md-5 post'></div>\n";
+if($poster_id == $_SESSION['uid']) {
+	print "<div class='col-md-1'>";
+	print "<a href=./postEdit.php?id=$item_id>Edit</a>\n";
+	print "</div>";
+	
+	print "<div class='col-md-1'>";
+	print "<div class='col-md-1'><a href='deletePost.php?op=landing&id=$item_id&cat=$category'>Delete</a></div>\n";
+	print "</div>";
+}
+print "</div>";
+
+print "<div class = 'row post'>";
+
+print "&nbsp &nbsp &nbsp &nbsp$detail<br/>\n";
+print "</div>";
+
+$uid = $_SESSION['uid'];
+$btnName = "claimBtn" . $i;
+$i++;
+print "<div class = 'row post'><div class='col-md-12'><br/></div></div>";
+print "<div class ='row post'><div class = 'col-md-1'></div><div class='col-md-10'>------------------------------------------------------------------------</div></div>\n";
+print  "<div class = 'row post'>";
+print "<div class = 'col-md-1'></div>";
+if($_SESSION['uid'] == null) {
+	$i = 0;
+print "<div class='col-md-2' align ='center'><a href='login.php'>Claim</a></div>";
+	print "<div class='col-md-1'> | </div>";
+	print "<div class='col-md-2'><a href='login.php'>Message</a></div>";  
+	print "</div>"; 
+	print "<div class ='row post'><div class = 'col-md-1'></div><div class='col-md-10'>------------------------------------------------------------------------</div></div>\n";
+}
+
+else{
+	print "<div class='col-md-2' align ='center' id='$btnName'><a href='landing.php?id=$item_id&uid=$uid'>Claim</a></div>\n";
+	print "<script>document.getElementById('$btnName').addEventListener('click', confirmClaim); </script>\n";
+	print "<div class='col-md-1'> | </div>";
+	print "<div class='col-md-2'><a href='handleButtons.php?op=sendmsg&sendid=$poster_id'>Message</a></div>\n";  
+	print "</div>"; 
+	print "<div class ='row post'><div class = 'col-md-1'></div><div class='col-md-10'>------------------------------------------------------------------------</div></div>\n";
+ }
+ print "<div class = 'row'><div class='col-md-12'><br/></div></div>\n";
 }
 
 ?>
