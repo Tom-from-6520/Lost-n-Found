42806141457de4d09f97f3f34798d344d0c071a9
Tom <cocplayer1alpha@gmail.com>
2021-12-30 00:28:51 -0500

    change the db file to hide password

--- db_connect.php
+++ db_connect.php
@@ -2,14 +2,12 @@
 $host="cray.cc.gettysburg.edu";
 $dbase="f21_2";
 $user = "doandu01";
-$pass = "doandu01";
+$pass = "someWrongPassword";
 
 try {
     $db = new PDO("mysql:host=$host;dbname=$dbase", $user, $pass);
 }
 catch(PDOException $e) {
-    // .  : string concatenation
-    // -> : remote access operator
     die("Error connecting to mysql server " . $e->getMessage());
 }
 ?>
